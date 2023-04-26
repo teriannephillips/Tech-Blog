4c025bfe7253431de987a768c71da1c9a646a45c
Teri Phillips <phillips.terianne@gmail.com>
2023-04-26 16:01:13 -0400

    corrections

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -19,8 +19,7 @@ async function loginFormHandler(event) {
         });
 
         if (response.ok) {
-            console.log("success!")
-            document.location.replace('/home');
+            document.location.replace('/');
         } else {
             alert(response.statusText);
         }
