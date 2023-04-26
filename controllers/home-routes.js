01fb514d64174073903b5e9cad6e12f8a98494ad
Teri Phillips <phillips.terianne@gmail.com>
2023-04-26 15:21:52 -0400

    created handlebars for home, login and signup

---
+++ controllers/home-routes.js
@@ -0,0 +1,40 @@
+const router = require('express').Router();
+
+router.get('/', (req, res) => {
+
+
+            res.render('home', {
+                loggedIn: req.session.loggedIn
+            });
+
+});
+router.get('/home', (req,res) => {
+    res.redirect('/');
+})
+
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+
+    res.render('login');
+});
+
+router.get('/signup', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+
+    res.render('signup');
+});
+
+
+router.get('*', (req, res) => {
+    res.status(404).send("Can't go there!");
+    // res.redirect('/');
+})
+
+
+module.exports = router;
